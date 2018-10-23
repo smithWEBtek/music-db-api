019ca1d39c963748d465a6ffa0b96eb7760223b2
smithwebtek <brad@smithwebtek.com>
2018-10-23 10:57:05 -0400

    Restore original code after solving Heroku error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
  
 	root 'api/songs#index'
 	get 'api/songs/:id/resources', to: 'api/songs#resources'
-	# get 'api/songs/:id/collections', to: 'api/songs#collections'
+	get 'api/songs/:id/collections', to: 'api/songs#collections'
 
 	namespace :api do 
 	  resources :categories
