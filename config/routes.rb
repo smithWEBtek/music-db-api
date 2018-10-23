a2e77352f4b6c0e02adbe1643a7cf1a3f48ed471
smithwebtek <brad@smithwebtek.com>
2018-10-23 10:33:15 -0400

    Comment out some routes, update ruby version

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
  
 	root 'api/songs#index'
-	get 'favicon.ico', to: 'api/songs#index'
-	get 'api/songs/:id/resources', to: 'api/songs#resources'
-	get 'api/songs/:id/collections', to: 'api/songs#collections'
+	# get 'favicon.ico', to: 'api/songs#index'
+	# get 'api/songs/:id/resources', to: 'api/songs#resources'
+	# get 'api/songs/:id/collections', to: 'api/songs#collections'
 
 	namespace :api do 
 	  resources :categories
